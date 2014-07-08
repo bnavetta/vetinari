ec4e5251449256139304e263b13cd43561a24ed3
Ben Navetta <ben.navetta@gmail.com>
2014-07-07 20:23:31 -0400

    license stuff

--- vetinari-core/src/main/java/com/bennavetta/vetinari/launch/ModuleLoader.java
+++ vetinari-core/src/main/java/com/bennavetta/vetinari/launch/ModuleLoader.java
@@ -1,6 +1,22 @@
+/**
+ * Copyright 2014 Ben Navetta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bennavetta.vetinari.launch;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterators;
@@ -32,7 +48,7 @@ public class ModuleLoader
 	private List<String> parseListing(URL listingUrl) throws IOException
 	{
 		return Resources.readLines(listingUrl, Charsets.UTF_8).stream().map(String::trim)
-				.filter(l -> !l.startsWith("#")).collect(Collectors.toList());
+				.filter(l -> !l.startsWith("#")).filter(l -> !Strings.isNullOrEmpty(l)).collect(Collectors.toList());
 	}
 
 	/**
